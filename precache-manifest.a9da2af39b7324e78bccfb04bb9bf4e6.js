7573fd0f7723dc54320e13d5ea5248d96d3fc66a
benRu <raxanon@gmail.com>
2020-11-06 22:51:41 +0100

    Updates

---
+++ precache-manifest.a9da2af39b7324e78bccfb04bb9bf4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1efb2c353e51458e38b89863e22f8cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2184c6537d720977e37b",
+    "url": "/static/css/main.a7a9b34b.chunk.css"
+  },
+  {
+    "revision": "c5fcdc847ac5637ffb41",
+    "url": "/static/js/2.90e2fc1a.chunk.js"
+  },
+  {
+    "revision": "c7fa27922e8659fd2fb918be015305bb",
+    "url": "/static/js/2.90e2fc1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2184c6537d720977e37b",
+    "url": "/static/js/main.722400f7.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  }
+]);
